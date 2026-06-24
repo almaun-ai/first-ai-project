33b0dcb5c9d4a5004574e46210c9e71a76e4aad4
almamun_ai <mamun1999735@gmail.com>
2026-06-24 22:23:56 +0900

    Multiple features model

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
 print("Hello Github")
 print("My first AI project")
 
+
+
+from sklearn.linear_model import LinearRegression
+import numpy as np
+
+x=np.array([[2,3],[4,5],[6,7],[8,9]])
+y=np.array([20,40,60,80])
+
+model = LinearRegression()
+model.fit(x,y)
+prediction = model.predict([[10,11]])
+print("prediction=",prediction)
